e428b295fbca05847e3b2622003e965682a789fa
starb520 <97459015+starb520@users.noreply.github.com>
2025-10-22 17:01:25 -0600

    working through the practice assignment

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,13 +11,19 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css'
 })
-export class RecipeDetailComponent {
-  @Input() recipe: Recipe;
+export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-  }
+    this.route.params.subscribe((params: Params) => { this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
+      });
+    }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
---
+++ src/app/recipes/recipe-start/recipe-start.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-start',
+  standalone: false,
+  templateUrl: './recipe-start.component.html',
+  styleUrl: './recipe-start.component.css'
+})
+export class RecipeStartComponent {
+
+}
